d1240c403f06903a22b6cf720340a079d5a90821
Antoine Remond-Tiedrez <a.remondtiedrez@gmail.com>
2026-01-23 16:02:25 -0600

    Updated the saved BoostedTrees model.
    The model was previously saved (following the xgboost default) as
    a binary JSON format (.ubj extension), but changes have now been made to
    be deliberate about saving that model as a JSON file (.json extension)
    instead.

--- hopus/demo/__init__.py
+++ hopus/demo/__init__.py
@@ -31,7 +31,7 @@ def load_trained_model(kind: str = "BoostedTrees") -> models.Model:
         path = resources.files("hopus").joinpath("demo/linear_regression.npz")
     elif kind == "BoostedTrees":
         model = models.BoostedTrees()
-        path = resources.files("hopus").joinpath("demo/boosted_trees.model")
+        path = resources.files("hopus").joinpath("demo/boosted_trees.json")
     else:
         raise ValueError(
             "`kind` must be either `Baseline`, `LinearRegression`, or `BoostedTrees`."
