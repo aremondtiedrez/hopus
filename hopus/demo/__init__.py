351ad57eb3036699684ca603a47f56700092c8be
Antoine Remond-Tiedrez <a.remondtiedrez@gmail.com>
2026-01-23 14:59:10 -0600

    Added trained models to the demo.
    Three trained models, one of each kind (Baseline, LinearRegression, and
    BoostedTrees) have been saved in the hopus/demo folder for easy access
    in the demo notebook.

--- hopus/demo/__init__.py
+++ hopus/demo/__init__.py
@@ -6,14 +6,35 @@ from importlib import resources
 
 import pandas as pd
 
+from .. import models
 
-def load_training_data():
+
+def load_training_data() -> pd.DataFrame:
     """Load the demo training data as a `pandas` `DataFrame`."""
     path = resources.files("hopus").joinpath("demo/training_data.csv")
     return pd.read_csv(path, index_col=0)
 
 
-def load_test_data():
+def load_test_data() -> pd.DataFrame:
     """Load the demo test data as a `pandas` `DataFrame`."""
     path = resources.files("hopus").joinpath("demo/test_data.csv")
     return pd.read_csv(path, index_col=0)
+
+
+def load_trained_model(kind: str = "BoostedTrees") -> models.Model:
+    """Load a trained demo model."""
+    if kind == "Baseline":
+        model = models.Baseline()
+        path = resources.files("hopus").joinpath("demo/baseline.csv")
+    elif kind == "LinearRegression":
+        model = models.LinearRegression()
+        path = resources.files("hopus").joinpath("demo/linear_regression.npz")
+    elif kind == "BoostedTrees":
+        model = models.BoostedTrees()
+        path = resources.files("hopus").joinpath("demo/boosted_trees.model")
+    else:
+        raise ValueError(
+            "`kind` must be either `Baseline`, `LinearRegression`, or `BoostedTrees`."
+        )
+    model.load(path)
+    return model
